20be47e67324f65452fe35f5dd6e61da398675b8
Giovanny Abrego <gm.abregoh@gmail.com>
2022-04-01 20:31:50 -0600

    content provider

---
+++ ContentProviderApp/app/src/main/java/sv/udb/edu/dsm/g05/contentproviderapp/MainActivity.java
@@ -0,0 +1,14 @@
+package sv.udb.edu.dsm.g05.contentproviderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
